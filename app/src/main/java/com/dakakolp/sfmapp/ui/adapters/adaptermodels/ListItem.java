b1d0dd4aa357b7717a2596876e05a4883342ffb4
daka-kolp <daka.kolp01@gmail.com>
2018-12-14 23:36:10 +0200

    ~97.5%

--- app/src/main/java/com/dakakolp/sfmapp/ui/adapters/adaptermodels/ListItem.java
+++ app/src/main/java/com/dakakolp/sfmapp/ui/adapters/adaptermodels/ListItem.java
@@ -6,40 +6,45 @@ public class ListItem {
     private String mSubtitle;
     private String mExtension;
     private File mFile;
-    public ListItem(int icon, String title, String subtitle, String ext, File file) {
-        this.mIcon = icon;
-        this.mTitle = title;
-        this.mSubtitle = subtitle;
-        this.mExtension = ext;
-        this.mFile = file;
-    }
+
+    public ListItem(){}
+
     public int getIcon() {
         return mIcon;
     }
+
     public void setIcon(int icon) {
         this.mIcon = icon;
     }
+
     public String getTitle() {
         return mTitle;
     }
+
     public void setTitle(String title) {
         this.mTitle = title;
     }
+
     public String getSubtitle() {
         return mSubtitle;
     }
+
     public void setSubtitle(String subtitle) {
         this.mSubtitle = subtitle;
     }
+
     public String getExtension() {
         return mExtension;
     }
+
     public void setExtension(String extension) {
         this.mExtension = extension;
     }
+
     public File getFile() {
         return mFile;
     }
+
     public void setFile(File file) {
         this.mFile = file;
     }
